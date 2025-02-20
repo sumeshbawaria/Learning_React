db8f0aca355566c0cdc212880b709104b45a6f98
sumeshbawaria <sumesh.bawaria1998@gmail.com>
2025-02-20 17:54:14 +0530

    Updated

--- 11_redux/src/component/Todos.jsx
+++ 11_redux/src/component/Todos.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {initializeTodo, removeTodo } from '../features/todo/todoSlice'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
     const todos = useSelector((state) => state.todos)
@@ -32,8 +32,8 @@ function Todos() {
             key={todo.id}
           >
             <div className='text-white'>{todo.text}</div>
-            <button
-             onClick={() => dispatch(removeTodo(todo.id))}
+            <button 
+            onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
             >
               <svg
